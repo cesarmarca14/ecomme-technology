0b9a9537bfd34c2902d0190d84748c2887f82f53
cesarmarca <cesar.14.2012@gmail.com>
2024-01-16 21:33:40 -0500

    ff

--- src/cesar/Categorias.jsx
+++ src/cesar/Categorias.jsx
@@ -3,14 +3,12 @@ import { categoriaApi } from "./Categorias1"
 const Categorias = () => {
   return (
     <div className="containerCategorias">
-<<<<<<< HEAD
       {categoriaApi.map((cesar)=>(
         <div>
             <img src={cesar.img} alt="" />
             <h2>{cesar.Id}</h2>
             
         </div>
-=======
       {categoriaApi.map((item)=>(
         <div>
             <img src={item.img} alt="" />
@@ -18,7 +16,6 @@ const Categorias = () => {
             
         </div>
      
->>>>>>> e532e6f0b922e9661ee634dc5a1afb45b1169866
 
       ))}
     </div>
