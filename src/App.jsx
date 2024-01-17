93cf66ff5e601c591d42ddea825e23b592234bdd
Cristhianmca <cristhianmc84@gmail.com>
2024-01-17 01:03:45 -0500

    malos cambios

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,17 @@ import Categoria from './cristhian/Categoria';
 import Novedades from './cristhian/Novedades';
 import Carrucel from './Carrucel.jsx';
 import Carru from './Carru.jsx';
-import { catnew } from './cristhian/apinew';
+
 import Slider from './cristhian/Slider.jsx';
 
 const App = () => {
-  const [search, setSearch] = useState('');
-
-  const filteredApiNew = catnew.filter(item =>
-    item.name.toLowerCase().includes(search.toLowerCase())
-  );
 
   return (
     <>
       <HeaderComputo />
-      <input
-        type="text"
-        placeholder="Buscar..."
-        value={search}
-        onChange={e => setSearch(e.target.value)}
-      />
       <Carru />
       <Categoria />
-      <Novedades items={filteredApiNew} />
-      {/* <Carrucel /> */}
+      <Novedades />
       <Slider />
     </>
   );
