cc440933d766c9b5e4e4dec18d6cfcd06119a28f
Cristhianmca <cristhianmc84@gmail.com>
2024-01-17 11:16:49 -0500

    juntando todo

---
+++ src/cristhian/HeaderComputo.jsx
@@ -0,0 +1,135 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import appFirebase from "../credenciales";
+import { getAuth, signOut } from "firebase/auth";
+
+const auth = getAuth(appFirebase);
+
+const HeaderComputo = ({ user }) => {
+  return (
+    <div className="w-[100%] mx-auto overflow-hidden bg-[#009fe3]  ">
+      <header>
+        <nav className="flex h-32 items-center justify-between">
+          <Link
+            to="/"
+            className="w-1/2  max-w-[400px] text-4xl text-white font-bold "
+          >
+            <img
+              className=" w-80 mt-6"
+              src="./public/DeluxeComputer2.png"
+              alt=""
+            />
+          </Link>
+          <input type="checkbox" id="menu" className=" peer/menu hidden" />
+          <label
+            htmlFor="menu"
+            className="hamburguer   w-10 h-10  bg-open-menu bg-cover bg-center rounded-lg  bg-blue-800 cursor-pointer peer-checked/menu:bg-close-menu transition-all z-40 md:hidden "
+          ></label>
+          <ul className="headersm fixed inset-0 bg-slate-500/80  text-lg text-white font-light px-[5%] grid auto-rows-max gap-8 content-center justify-items-center clip-circle-0 peer-checked/menu:clip-circle-full transition-[clip-path] duration-500 md:clip-circle-full md:relative md:grid-flow-col md:p-4 md:bg-transparent ">
+            {user ? (
+              <li className="py-1 px-1 rounded-lg hover:font-bold">
+                <p
+                  className="inicio font-semibold flex flex-col items-center"
+                  to="/login"
+                >
+                  {" "}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  Bienvenido {user?.email}
+                </p>
+              </li>
+            ) : (
+              <li className="py-1 px-1 rounded-lg hover:font-bold">
+                <Link
+                  className="inicio font-semibold flex flex-col items-center"
+                  to="/login"
+                >
+                  {" "}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  Inicio Sesion
+                </Link>
+              </li>
+            )}
+
+            {/* <li className=" py-1 px-1 rounded-lg  hover:font-bold">
+                <a className="pedidos font-semibold" href="#">| Pedidos</a>
+            </li> */}
+            <li className=" py-1 px-1 rounded-lg ">
+              <a
+                className="carrito font-semibold flex
+                flex-col items-center"
+                href="#"
+              >
+                {" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path d="M5.223 2.25c-.497 0-.974.198-1.325.55l-1.3 1.298A3.75 3.75 0 0 0 7.5 9.75c.627.47 1.406.75 2.25.75.844 0 1.624-.28 2.25-.75.626.47 1.406.75 2.25.75.844 0 1.623-.28 2.25-.75a3.75 3.75 0 0 0 4.902-5.652l-1.3-1.299a1.875 1.875 0 0 0-1.325-.549H5.223Z" />
+                  <path
+                    fillRule="evenodd"
+                    d="M3 20.25v-8.755c1.42.674 3.08.673 4.5 0A5.234 5.234 0 0 0 9.75 12c.804 0 1.568-.182 2.25-.506a5.234 5.234 0 0 0 2.25.506c.804 0 1.567-.182 2.25-.506 1.42.674 3.08.675 4.5.001v8.755h.75a.75.75 0 0 1 0 1.5H2.25a.75.75 0 0 1 0-1.5H3Zm3-6a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75v3a.75.75 0 0 1-.75.75h-3a.75.75 0 0 1-.75-.75v-3Zm8.25-.75a.75.75 0 0 0-.75.75v5.25c0 .414.336.75.75.75h3a.75.75 0 0 0 .75-.75v-5.25a.75.75 0 0 0-.75-.75h-3Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                | Carrito de compras
+              </a>
+            </li>
+
+            {user && (
+              <li className=" py-1 px-1 rounded-lg ">
+                <button
+                  className="carrito font-semibold flex
+                flex-col items-center"
+                  onClick={() => signOut(auth)}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path d="M5.223 2.25c-.497 0-.974.198-1.325.55l-1.3 1.298A3.75 3.75 0 0 0 7.5 9.75c.627.47 1.406.75 2.25.75.844 0 1.624-.28 2.25-.75.626.47 1.406.75 2.25.75.844 0 1.623-.28 2.25-.75a3.75 3.75 0 0 0 4.902-5.652l-1.3-1.299a1.875 1.875 0 0 0-1.325-.549H5.223Z" />
+                    <path
+                      fillRule="evenodd"
+                      d="M3 20.25v-8.755c1.42.674 3.08.673 4.5 0A5.234 5.234 0 0 0 9.75 12c.804 0 1.568-.182 2.25-.506a5.234 5.234 0 0 0 2.25.506c.804 0 1.567-.182 2.25-.506 1.42.674 3.08.675 4.5.001v8.755h.75a.75.75 0 0 1 0 1.5H2.25a.75.75 0 0 1 0-1.5H3Zm3-6a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75v3a.75.75 0 0 1-.75.75h-3a.75.75 0 0 1-.75-.75v-3Zm8.25-.75a.75.75 0 0 0-.75.75v5.25c0 .414.336.75.75.75h3a.75.75 0 0 0 .75-.75v-5.25a.75.75 0 0 0-.75-.75h-3Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  | Salir
+                </button>
+              </li>
+            )}
+            <li>
+              <a className="hover:font-bold" href="#"></a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+};
+export default HeaderComputo;
---
+++ src/cristhian/apinew.js
@@ -0,0 +1,99 @@
+export const catnew = [
+    {
+        Id: 1,
+        name: 'Logitech POP Keys Teclado inalámbrico mecánico con teclas de emojis personalizables',
+        informacion: 'Logitech POP Keys Teclado inalámbrico mecánico con teclas de emojis personalizables, diseño compacto duradero, conectividad Bluetooth o USB, multidispositivo, compatible con varios SO, amarillo Blast,...',
+        price: '$66.00',
+        marca: 'Logitech',
+        img: 'https://i.postimg.cc/W4VS0s9w/logitrans.png',
+
+
+
+    },
+    {
+        id: 2,
+        name: 'Logitech G502 HERO',
+        informacion: 'Logitech G502 HERO - Mouse para juegos de alto rendimiento con cable, sensor HERO 25K, 25,600 ppp, RGB, pesos ajustables, 11 botones programables, memoria...',
+        price: '$48.58',
+        marca: 'Logitech',
+        img: 'https://i.postimg.cc/JhVsYnJS/Post-de-instagram-de-venta-de-tecnolog-a-morado-con-azul-1.png',
+    },
+    {
+        id: 3,
+        name:'ASUS Asus VG278QR 27',
+        informacion:'ASUS Asus VG278QR 27 "Monitor de juego, 1080P Full HD, 165Hz (soportes 144Hz), compatible con G-sync, 0.5 ms, desenfoque de movimiento bajo extremo,...',
+        price: '$186.99',
+        marca: 'ASUS',
+        img: 'https://i.postimg.cc/Jzcsg8qn/Post-de-instagram-de-venta-de-tecnolog-a-morado-con-azul-2.png'
+
+    },
+    {
+        id: 4,
+        name: 'Intel® Core™ i9-14900KF',
+        informacion: 'Intel® Nuevo procesador de escritorio para juegos Core™ i9-14900KF de 24 núcleos (8 núcleos P + 16 núcleos E) - Desbloqueado',
+        price: '$544.03',
+        marca: 'Intel',
+        img:'https://i.postimg.cc/3NNpQx17/Post-de-instagram-de-venta-de-tecnolog-a-morado-con-azul-3.png'
+
+    },
+    {
+        id: 5,
+        name: 'ASUS TUF Gaming VG28UQL1A',
+        informacion:'Monitor de juegos de 28 pulgadas, resolución 4K UHD (3840 x 2160), 144 HZ, DSC HDMI 2.1, panel IPS rápido, 1 ms, Extreme Low Motion Blur Sync, G-SYNC, FreeSync Premium, Eye Care, DCI-P3 del 90 %',
+        price: '$599.99',
+        marca: 'ASUS',
+        img:'https://i.postimg.cc/PxQWW73p/Post-de-instagram-de-venta-de-tecnolog-a-morado-con-azul-4.png'
+    },
+    {
+        id: 6,
+        name: 'STGAubron PC de escritorio para juegos',
+        informacion:'Intel Core i7 3.4G hasta 3.9G, 16G RAM, 512G SSD, Radeon RX 580 16G GDDR5, 600M WiFi, BT 5.0, ventilador RGB x 2, teclado y mouse RGB, alfombrilla de mouse RGB, barra de sonido RGB BT, W10H64',
+        price: '$479.99',
+        marca: 'Skytech Gaming',
+        img:'https://i.postimg.cc/zfFmhzrn/Post-de-instagram-de-venta-de-tecnolog-a-morado-con-azul-5.png'
+    },
+    {
+        id: 7,
+        name: 'Kingston memoria de escritorio DDR5 CL36 RGB de 16 GB',
+        informacion:'Kingston Technology Fury Beast Kit de memoria de escritorio DDR5 CL36 RGB de 16 GB (2 x 8 GB) 5600MT/s | Sincronización infrarroja | AMD Expo | Plug N Play | KF556C36BBEAK2-16',
+        price: '$93.27',
+        marca: 'Kingston',
+        img:'https://i.postimg.cc/52cLY1p9/Post-de-instagram-de-venta-de-tecnolog-a-morado-con-azul-6.png'
+    },
+    {
+        id: 8,
+        name: 'Redragon K580 VATA RGB LED',
+        informacion:'Redragon Teclado mecánico para juegos K580 VATA RGB LED retroiluminado con teclas macro y controles multimedia dedicados, grabación macro integrada (interruptores marrones)',
+        price: '$69.99',
+        marca: 'RedDragon',
+        img:'https://i.postimg.cc/k5gZ2kn4/Post-de-instagram-de-venta-de-tecnolog-a-morado-con-azul-7.png'
+    },
+    {
+        id: 9,
+        name: 'REDRAGON S101 Set combo',
+        informacion:'Combo de teclado y mouse para juegos de PC: incluye teclado Redragon RGB retroiluminado para juegos y mouse para juegos con retroiluminación RGB. Kit de PC todo en uno, fantástico para jugadores (nueva versión mejorada)',
+        price: '$36.88',
+        marca: 'RedDragon',
+        img:'https://i.postimg.cc/N086wMh2/Post-de-instagram-de-venta-de-tecnolog-a-morado-con-azul-8.png'
+
+    },
+    {
+        id: 10,
+        name: 'Acer Nitro 5 AN515-58-525P - Laptop para juegos',
+        informacion:'Core i5-12500H | GPU NVIDIA GeForce RTX 3050 | Pantalla IPS FHD de 15.6 pulgadas de 144 Hz | DDR4 de 8 GB | SSD PCIe Gen 4 de 512 GB | Killer Wi-Fi 6 | Teclado retroiluminado, negro',
+        price: '$689.67',
+        marca: 'Acer-Nitro',
+        img:'https://i.postimg.cc/DZkbmKhk/Post-de-instagram-de-venta-de-tecnolog-a-morado-con-azul-9.png'
+    },
+    {
+        id: 10,
+        name: 'Ryzen 7 5800X de 8 núcleos y 16 hilos',
+        informacion:'AMD Procesador de escritorio desbloqueado Ryzen 7 5800X de 8 núcleos y 16 hilos',
+        price: '$234.43',
+        marca: 'AMD',
+        img:'https://i.postimg.cc/VkZVxK8b/Post-de-instagram-de-venta-de-tecnolog-a-morado-con-azul-10.png'
+    }
+
+
+
+]
