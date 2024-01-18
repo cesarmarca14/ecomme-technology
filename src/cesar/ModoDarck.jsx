5156339cd13059179ae2f4a4e8f2090126dc0a2c
Cristhianmca <cristhianmc84@gmail.com>
2024-01-17 20:48:25 -0500

    despues del pull

--- src/cesar/ModoDarck.jsx
+++ src/cesar/ModoDarck.jsx
@@ -13,19 +13,9 @@ const ModoDarck = () => {
 
   return (
     <>
-<<<<<<< HEAD
-    
-  <>
-    <button className='' onClick={toggle}>
-      {Darck ? <i className="fs-3 bi-moon"></i> : <i className="fs-3  bi-sun-fill"></i>}
-    </button>
-  </>
-
-=======
       <button onClick={toggle}>
         {Darck ? <i className="fs-3 noche bi-moon"></i> : <i className="fs-2 dia bi-sun-fill"></i>}
       </button>
->>>>>>> 1dca1a4f0de8db0aaa877f8662c84bd289bda288
     </>
   );
 };
