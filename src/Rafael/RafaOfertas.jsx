8ef52b960e5542c907be6b6a76f067bd42f290be
rosnayo <rafaelosnayo@hotmail.com>
2024-01-29 08:53:41 -0500

    corregido5

--- src/Rafael/RafaOfertas.jsx
+++ src/Rafael/RafaOfertas.jsx
@@ -4,6 +4,7 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import { getDataofertasApi } from "./rafaApi.js";
 import "./RafaOfertas.css";
+import {Carousel} from 'react-bootstrap'
 
 const RafaOfertas = () => {
   // vamos crea una variable para guaradr la lista de ofertas
@@ -14,6 +15,31 @@ const RafaOfertas = () => {
 
   return (
     <>
+    <Carousel>
+        <Carousel.Item interval={1000}>
+          <br /><br /><br /><br /><br />
+          <img 
+          //  size-full md:size-auto
+          src="./src/Rafael/imagen/carrusel01.jpg" 
+          alt="First Slide"/>
+          <Carousel.Caption>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={500}>
+        <br /><br /><br /><br /><br />
+        <img src="./src/Rafael/imagen/carrusel02.jpg" alt="Second Slide"/>
+          <Carousel.Caption>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+        <br /><br /><br /><br /><br />
+        <img 
+        src="./src/Rafael/imagen/carrusel03.jpg" alt="Third Slide"/>
+          <Carousel.Caption>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+
       <Row xs={1} md={3} className="g-4">
         {getDataofertasApi.map((getDataofertasApi, idx) => (
           <Col key={idx}>
